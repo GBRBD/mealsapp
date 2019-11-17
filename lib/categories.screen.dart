346ab1b5474cd5aad8e7bd03de1eb570ae58784c
GBRBD <gaborattilabodi@gmail.com>
2019-11-17 21:15:10 +0100

    Create category item, show dummy data in categories screen

--- lib/categories.screen.dart
+++ lib/categories.screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mealsapp/category_item.dart';
+
+import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[],
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(
+                title: catData.title,
+                color: catData.color,
+              ))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
