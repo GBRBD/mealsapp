4dab2698b1a76bc691d4202c68bc8dd1646f1619
GBRBD <gaborattilabodi@gmail.com>
2019-11-25 21:28:27 +0100

    Add Bottom tab bar with shifting instead of top bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,33 +8,76 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  List<Widget> _pages = [CategoriesScreen(), FavoritesScreen()];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        unselectedItemColor: Colors.white,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).accentColor,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
       ),
     );
   }
+
+  //Top tabbar
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return DefaultTabController(
+  //     length: 2,
+  //     child: Scaffold(
+  //       appBar: AppBar(
+  //         title: Text('Meals'),
+  //         bottom: TabBar(
+
+  //           tabs: <Widget>[
+  //             Tab(
+  //               icon: Icon(Icons.category),
+  //               text: 'Categories',
+  //             ),
+  //             Tab(
+  //               icon: Icon(Icons.star),
+  //               text: 'Favorites',
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //       body: TabBarView(
+  //         children: <Widget>[
+  //           CategoriesScreen(),
+  //           FavoritesScreen(),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
 }
