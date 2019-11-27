3eaa810fe71ab50ae264fd96b22087af380cf292
GBRBD <gaborattilabodi@gmail.com>
2019-11-27 21:34:33 +0100

    Replacing Pages (Instead of Pushing)

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:mealsapp/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const String routeName = '/filters';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('FIlters!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your favorites'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('FIlters!'),
+      ),
     );
   }
 }
